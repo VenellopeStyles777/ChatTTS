14b1bc31bb3c35c6fb871039ab1b3416e3d60f7d
VenellopeStyles777 <chrisqi@usc.edu>
2024-05-29 16:51:49 +0800

    Update test.py

--- ChatTTS/experimental/test.py
+++ ChatTTS/experimental/test.py
@@ -1,28 +1,10 @@
-###################################
-# Sample a speaker from Gaussian.
-import torch
-std, mean = torch.load('ChatTTS/asset/spk_stat.pt').chunk(2)
-rand_spk = torch.randn(768) * std + mean
+import ChatTTS
+from IPython.display import Audio
 
-params_infer_code = {
-  'spk_emb': rand_spk, # add sampled speaker 
-  'temperature': .3, # using custom temperature
-  'top_P': 0.7, # top P decode
-  'top_K': 20, # top K decode
-}
+chat = ChatTTS.Chat()
+chat.load_models()
 
-###################################
-# For sentence level manual control.
+texts = ["<PUT YOUR TEXT HERE>",]
 
-# use oral_(0-9), laugh_(0-2), break_(0-7) 
-# to generate special token in text to synthesize.
-params_refine_text = {
-  'prompt': '[oral_2][laugh_0][break_6]'
-} 
-
-wav = chat.infer("<PUT YOUR TEXT HERE>", params_refine_text=params_refine_text, params_infer_code=params_infer_code)
-
-###################################
-# For word level manual control.
-text = 'What is [uv_break]your favorite english food?[laugh][lbreak]'
-wav = chat.infer(text, skip_refine_text=True, params_infer_code=params_infer_code)
+wavs = chat.infer(texts, use_decoder=True)
+Audio(wavs[0], rate=24_000, autoplay=True)
